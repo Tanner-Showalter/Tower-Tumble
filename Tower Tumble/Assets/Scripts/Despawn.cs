3d4590769a6b42c4d3783e2beae9fa96beca8a40
sethcwiley <sethcwiley@gmail.com>
2024-04-15 10:34:07 -0500

    Despawn Code
    Code to despawn the balls after 10 seconds + shrunk background

--- Tower Tumble/Assets/Scripts/Despawn.cs
+++ Tower Tumble/Assets/Scripts/Despawn.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Despawn : MonoBehaviour
 {
     private float lowerYBound = -0;
+    private Coroutine timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = StartCoroutine(loseCoroutine());
+    }
+    
+    IEnumerator loseCoroutine()
+    {
+
+        //yield on a new YieldInstruction that waits for 10 seconds.
+        yield return new WaitForSeconds(10);
+        // In plain english, wait 10 seconds before continuing
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
