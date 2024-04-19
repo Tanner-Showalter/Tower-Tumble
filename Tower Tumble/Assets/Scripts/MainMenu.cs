c54a900c5e7d1467c3c68f5e0d2cb832c7d63d0b
sethcwiley <sethcwiley@gmail.com>
2024-04-18 22:17:15 -0500

    Levels 3-4
    Added two more levels with green and blue blocks.

--- Tower Tumble/Assets/Scripts/MainMenu.cs
+++ Tower Tumble/Assets/Scripts/MainMenu.cs
@@ -13,4 +13,12 @@ public class MainMenu : MonoBehaviour
     public void PlayLevel2() {
         SceneManager.LoadSceneAsync("Level 2");
     }
+    
+    public void PlayLevel3() {
+        SceneManager.LoadSceneAsync("Level 3");
+    }
+    
+    public void PlayLevel4() {
+        SceneManager.LoadSceneAsync("Level 4");
+    }
 }
